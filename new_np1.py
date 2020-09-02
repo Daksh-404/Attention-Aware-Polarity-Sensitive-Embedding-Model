2645938c2b854e4bff11bbc98f0b873fabf94a27
Daksh Gupta <61038855+Daksh-404@users.noreply.github.com>
2020-09-02 20:11:50 +0530

    Update new_np1.py

--- new_np1.py
+++ new_np1.py
@@ -22,51 +22,63 @@ class NpairLoss(nn.Module):
         indices12 = torch.LongTensor([1, 5, 9, 13, 17, 21, 25, 29])
         indices21 = torch.LongTensor([2, 6, 10, 14, 18, 22, 26, 30])
         indices22 = torch.LongTensor([3, 7, 11, 15, 19, 23, 27, 31])
-        anchor1 = torch.index_select(embeddings, 0, indices11)
-        positive1 = torch.index_select(embeddings, 0, indices12)
-        anchor2 = torch.index_select(embeddings, 0, indices21)                      ####CHANGED######
-        positive2 = torch.index_select(embeddings, 0, indices22)
+        anchor1 = torch.index_select(embeddings, 0, indices11.cuda())
+        positive1 = torch.index_select(embeddings, 0, indices12.cuda())
+        anchor2 = torch.index_select(embeddings, 0, indices21.cuda())                      ####CHANGED######
+        positive2 = torch.index_select(embeddings, 0, indices22.cuda())
         batch_size = anchor1.size(0)
-        target = target.view(target.size(0), 1)
+        target = target.view(target.size(0)*target.size(1), 1)
         target = torch.index_select(target, 0, indices11)
-
         polarity_score = n2
         emotion_score = n4
         polarity = target < 4
+        anchor1_score_p = torch.index_select(polarity_score, 0, indices11.cuda())
+        #print(anchor1_score_p)
+        positive1_score_p = torch.index_select(polarity_score, 0, indices12.cuda())                ###CHANGED###
+        anchor2_score_p = torch.index_select(polarity_score, 0, indices21.cuda())
+        positive2_score_p = torch.index_select(polarity_score, 0, indices22.cuda())
 
-        anchor1_score_p = torch.index_select(polarity_score, 0, indices11)
-        positive1_score_p = torch.index_select(polarity_score, 0, indices12)                ###CHANGED###
-        anchor2_score_p = torch.index_select(polarity_score, 0, indices21)
-        positive2_score_p = torch.index_select(polarity_score, 0, indices22)
+        anchor1_score_e = torch.index_select(emotion_score, 0, indices11.cuda()).long()
+        positive1_score_e = torch.index_select(emotion_score, 0, indices12.cuda()).long()              ###CHANGED####
+        anchor2_score_e = torch.index_select(emotion_score, 0, indices21.cuda()).long()
+        positive2_score_e = torch.index_select(emotion_score, 0, indices22.cuda()).long()
 
-        anchor1_score_e = torch.index_select(emotion_score, 0, indices11)
-        positive1_score_e = torch.index_select(emotion_score, 0, indices12)                 ###CHANGED####
-        anchor2_score_e = torch.index_select(emotion_score, 0, indices21)
-        positive2_score_e = torch.index_select(emotion_score, 0, indices22)
+        weight_polarity_anchor1 = torch.zeros(8, 2)
+        weight_polarity_positive1 = torch.zeros(8, 2)
+        weight_polarity_anchor2 = torch.zeros(8, 2)
+        weight_polarity_positive2 = torch.zeros(8, 2)
 
-        weight_polarity_anchor1 = torch.zeros(8, 8)
-        weight_polarity_positive1 = torch.zeros(8, 8)
-        weight_polarity_anchor2 = torch.zeros(8, 8)
-        weight_polarity_positive2 = torch.zeros(8, 8)
+        #print(polarity)
 
+        prev=True
+        main=polarity.squeeze().data.cpu().numpy()
+        x=0
+        for x in range(main.shape[0]-1):
+          if main[x]==False and main[x+1]==False:
+            pass
+          else:
+            break
+
+        main=np.array([main[x],main[x+1]])
         for i in range(8):
-            weight_polarity_anchor1[i] = anchor1_score_p[i, polarity.squeeze().data.cpu().numpy()]
-            weight_polarity_positive1[i] = positive1_score_p[i, polarity.squeeze().data.cpu().numpy()]
-            weight_polarity_anchor2[i] = anchor2_score_p[i, polarity.squeeze().data.cpu().numpy()]
-            weight_polarity_positive2[i] = positive2_score_p[i, polarity.squeeze().data.cpu().numpy()]
+           # print(anchor1_score_p[i,polarity.squeeze().data.cpu().numpy()].size())
+            weight_polarity_anchor1[i] = anchor1_score_p[i, main]
+            weight_polarity_positive1[i] = positive1_score_p[i, main]
+            weight_polarity_anchor2[i] = anchor2_score_p[i, main]
+            weight_polarity_positive2[i] = positive2_score_p[i, main]
 
-        weight_emotion_anchor1 = torch.zeros(8, 8)
-        weight_emotion_positive1 = torch.zeros(8, 8)
-        weight_emotion_anchor2 = torch.zeros(8, 8)
-        weight_emotion_positive2 = torch.zeros(8, 8)
+        weight_emotion_anchor1 = torch.zeros(8, 8).long()
+        weight_emotion_positive1 = torch.zeros(8, 8).long()
+        weight_emotion_anchor2 = torch.zeros(8, 8).long()
+        weight_emotion_positive2 = torch.zeros(8, 8).long()
 
+        m=target.long()
         for i in range(8):
-            weight_emotion_anchor1[i] = anchor1_score_e[i][target].squeeze()
-            weight_emotion_positive1[i] = positive1_score_e[i][target].squeeze()
-            weight_emotion_anchor2[i] = anchor2_score_e[i][target].squeeze()
-            weight_emotion_positive2[i] = positive2_score_e[i][target].squeeze()
-
-        tgidx = target
+            weight_emotion_anchor1[i] = anchor1_score_e[i][m].squeeze()
+            weight_emotion_positive1[i] = positive1_score_e[i][m].squeeze()
+            weight_emotion_anchor2[i] = anchor2_score_e[i][m].squeeze()
+            weight_emotion_positive2[i] = positive2_score_e[i][m].squeeze()
+        tgidx = target.long()
         label = torch.LongTensor([0, 0, 0, 0, 0, 0, 0, 0])
         label = label.view(label.size(0), 1)
         for i in range(0, 8):
@@ -78,9 +90,9 @@ class NpairLoss(nn.Module):
         logit1 = torch.matmul(anchor1, torch.transpose(positive1, 0, 1))
         logit2 = torch.matmul(anchor2, torch.transpose(positive2, 0, 1))
 
-
-        weight_polarity1 = torch.exp(weight_polarity_anchor1) * torch.exp(torch.transpose(weight_polarity_positive1, 0, 1))
-        weight_polarity2 = torch.exp(weight_polarity_anchor2) * torch.exp(torch.transpose(weight_polarity_positive2, 0, 1))
+       # print(weight_polarity_anchor1.size()," ",weight_polarity_positive1.size())
+        weight_polarity1 = torch.matmul(torch.exp(weight_polarity_anchor1), torch.exp(torch.transpose(weight_polarity_positive1, 0, 1)))
+        weight_polarity2 = torch.matmul(torch.exp(weight_polarity_anchor2), torch.exp(torch.transpose(weight_polarity_positive2, 0, 1)))
 
         for i in range(0, 8):
             for j in range(0, 8):
@@ -89,23 +101,23 @@ class NpairLoss(nn.Module):
                     weight_polarity2[i][j] = 1
 
 
-        weight_emotion1 = torch.exp(weight_emotion_anchor1) * torch.exp(torch.transpose(weight_emotion_positive1, 0, 1))
-        weight_emotion2 = torch.exp(weight_emotion_anchor2) * torch.exp(torch.transpose(weight_emotion_positive2, 0, 1))
+        weight_emotion1 = torch.matmul(torch.exp(weight_emotion_anchor1.float()), torch.exp(torch.transpose(weight_emotion_positive1.float(), 0, 1)))
+        weight_emotion2 = torch.matmul(torch.exp(weight_emotion_anchor2.float()), torch.exp(torch.transpose(weight_emotion_positive2.float(), 0, 1)))
 
 
         for i in range(0, 8):
             weight_emotion1[i][i] = 1
             weight_emotion2[i][i] = 1
 
-        logit1 = logit1 * weight_polarity1
-        logit2 = logit2 * weight_polarity2     ########## CHANGED###
+        logit1 = logit1 * weight_polarity1.cuda()
+        logit2 = logit2 * weight_polarity2.cuda()     ########## CHANGED###
 
-        logit1 = logit1 * weight_emotion1
-        logit2 = logit2 * weight_emotion2
+        logit1 = logit1 * weight_emotion1.cuda()
+        logit2 = logit2 * weight_emotion2.cuda()
 
 
-        logit1_2 = torch.zeros(8, 2)            ###CHANGED###
-        logit2_2 = torch.zeros(8, 2)
+        logit1_2 = torch.zeros(8, 2).cuda()            ###CHANGED###
+        logit2_2 = torch.zeros(8, 2).cuda()
 
         index_1 = torch.LongTensor([[label[0], label[1], label[2], label[3]]])
         index_2 = torch.LongTensor([[label[4], label[5], label[6], label[7]]])
@@ -130,9 +142,9 @@ class NpairLoss(nn.Module):
                 logit2_2[i, 1] = (logit2[i, label[0:4]].sum())/4
 
 
-        tg2 = torch.zeros(8, 2)     #CHANGED###
+        tg2 = torch.zeros(8, 2).cuda()     #CHANGED###
         tg2[:, 0] = 1
-        tg3 = torch.cat([torch.eye(4), torch.eye(4)], 0)      ###CHANGED##
+        tg3 = torch.cat([torch.eye(4), torch.eye(4)], 0).cuda()      ###CHANGED##
 
         loss_ce1 = 0.5 * cross_entropy(logit1_2, tg2) + 0.5 * cross_entropy(lossp1, tg3)
         loss_ce2 = 0.5 * cross_entropy(logit2_2, tg2) + 0.5 * cross_entropy(lossp2, tg3)
